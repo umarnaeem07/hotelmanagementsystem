6c2909532c310b2651ce5f9511ba347bde043cbb
Umar <mbaig2853@gmail.com>
2026-06-15 00:27:53 +0500

    adding mail invitation

--- config/settings.py
+++ config/settings.py
@@ -15,10 +15,10 @@ import os
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -171,3 +171,16 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     ".ngrok-free.dev",
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL"
+)
+FRONTEND_URL = os.getenv("FRONTEND_URL")
--- staff/views.py
+++ staff/views.py
@@ -2,10 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.conf import settings
 from .models import StaffInvitation
 from .serializers import StaffInvitationSerializer
-
+from django.core.mail import send_mail
 
 class StaffInvitationAPIView(APIView):
 
@@ -38,6 +38,25 @@ class StaffInvitationAPIView(APIView):
         invitation = serializer.save(
             hotel=request.user.hotel,
             invited_by=request.user
+        )
+        # Create invitation link
+        invite_link = (
+            f"{settings.FRONTEND_URL}/"
+            f"accept-invitation/{invitation.token}"
+        )
+        # Send email
+        send_mail(
+            subject="Hotel Staff Invitation",
+            message=(
+                f"You have been invited to join "
+                f"{request.user.hotel.name}.\n\n"
+                f"Role: {invitation.role}\n\n"
+                f"Accept your invitation here:\n"
+                f"{invite_link}"
+            ),
+            from_email=None,  # Uses DEFAULT_FROM_EMAIL from settings.py
+            recipient_list=[invitation.email],
+            fail_silently=False,
         )
 
         return Response(
